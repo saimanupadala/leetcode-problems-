4ade86b7d8a6ffd473754745ea1ffc91cc186187
saimanupadala <saimanupadala@gmail.com>
2026-08-05 12:06:32 +0530

    Time: 0 ms (100%), Space: 19.6 MB (41.55%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,13 @@
+class Solution:
+    def flatten(self, root):
+        self.prev = None
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.right)
+            dfs(node.left)
+            node.right = self.prev
+            node.left = None
+            self.prev = node
+        dfs(root)
+        
