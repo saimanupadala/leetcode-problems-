87dcb7ef53991b2e076616e91fe6aaf7702945ed
saimanupadala <saimanupadala@gmail.com>
2026-08-09 21:45:13 +0530

    Time: 0 ms (100%), Space: 27.7 MB (80.12%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        second = prev
+        first = head
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
