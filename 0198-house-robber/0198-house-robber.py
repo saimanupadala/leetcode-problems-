930612c4724c7bd38967840f860979f3fb649b4e
saimanupadala <saimanupadala@gmail.com>
2026-08-13 20:44:11 +0530

    Time: 0 ms (100%), Space: 19.1 MB (98.65%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        prev2 = 0
+        prev1 = 0
+        for money in nums:
+            current = max(prev1, prev2 + money)
+            prev2 = prev1
+            prev1 = current
+        return prev1
+        
