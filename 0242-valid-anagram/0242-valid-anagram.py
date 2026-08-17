8d14f076491d9388cc44e0f17bced00eccd6d4fd
saimanupadala <saimanupadala@gmail.com>
2026-08-17 09:55:33 +0530

    Time: 14 ms (48.12%), Space: 19.4 MB (75.92%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        return all(x == 0 for x in count)
+        
