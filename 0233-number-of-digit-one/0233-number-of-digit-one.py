2cc663663e8ca8fa186865ce23d59218fe9cd79f
saimanupadala <saimanupadala@gmail.com>
2026-08-17 09:36:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (29.75%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        count = 0
+        factor = 1
+
+        while factor <= n:
+            higher = n // (factor * 10)
+            current = (n // factor) % 10
+            lower = n % factor
+
+            if current == 0:
+                count += higher * factor
+            elif current == 1:
+                count += higher * factor + lower + 1
+            else:
+                count += (higher + 1) * factor
+
+            factor *= 10
+
+        return count
+        
