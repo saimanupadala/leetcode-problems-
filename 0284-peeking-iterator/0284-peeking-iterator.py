1e4df0abae14d6acc8c70d9bdb8fef4ef7766c8c
saimanupadala <saimanupadala@gmail.com>
2026-08-24 21:33:12 +0530

    Time: 42 ms (81.67%), Space: 19.5 MB (88.06%) - LeetHub

---
+++ 0284-peeking-iterator/0284-peeking-iterator.py
@@ -0,0 +1,23 @@
+class PeekingIterator:
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self.peeked = None
+        self.has_peeked = False
+
+    def peek(self):
+        if not self.has_peeked:
+            self.peeked = self.iterator.next()
+            self.has_peeked = True
+        return self.peeked
+
+    def next(self):
+        if self.has_peeked:
+            value = self.peeked
+            self.peeked = None
+            self.has_peeked = False
+            return value
+
+        return self.iterator.next()
+
+    def hasNext(self):
+        return self.has_peeked or self.iterator.hasNext()
