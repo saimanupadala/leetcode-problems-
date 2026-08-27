e80e2caefc545f2c9aabaf57acde8e75d68c5f97
saimanupadala <saimanupadala@gmail.com>
2026-08-27 22:37:29 +0530

    Time: 52 ms (44.45%), Space: 19.5 MB (14.58%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combinationSum4(self, nums, target):
+        dp = [0] * (target + 1)
+
+    
+        dp[0] = 1
+
+        for total in range(1, target + 1):
+            for num in nums:
+                if num <= total:
+                    dp[total] += dp[total - num]
+
+        return dp[target]
+        
