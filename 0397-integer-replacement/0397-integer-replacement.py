23ffd29b5dc4e8c3f2cb9aae0b60e040cb21f55b
saimanupadala <saimanupadala@gmail.com>
2026-08-30 18:02:57 +0530

    Time: 0 ms (100%), Space: 19.3 MB (70.05%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def integerReplacement(self, n):
+        count = 0
+
+        while n != 1:
+            if n % 2 == 0:
+                n //= 2
+
+            elif n == 3:
+                n -= 1
+
+            elif n % 4 == 1:
+                n -= 1
+
+            else:
+                n += 1
+
+            count += 1
+
+        return count
+        
