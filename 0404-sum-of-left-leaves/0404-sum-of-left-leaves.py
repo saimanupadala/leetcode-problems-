e8c2165993a4d6285cdfd3c6805bbb7b923da57d
saimanupadala <saimanupadala@gmail.com>
2026-08-30 18:17:02 +0530

    Time: 0 ms (100%), Space: 19.4 MB (65.64%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sumOfLeftLeaves(self, root):
+
+        if root is None:
+            return 0
+
+        total = 0
+
+    
+        if root.left:
+
+          
+            if root.left.left is None and root.left.right is None:
+                total += root.left.val
+
+            else:
+                total += self.sumOfLeftLeaves(root.left)
+
+      
+        total += self.sumOfLeftLeaves(root.right)
+
+        return total
