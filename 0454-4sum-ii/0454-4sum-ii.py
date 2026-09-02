a07cae2f3a62edf844d3f69409e22cea50ac98a3
saimanupadala <saimanupadala@gmail.com>
2026-09-02 22:22:24 +0530

    Time: 396 ms (44.57%), Space: 19.5 MB (49.32%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        count = {}
+
+        for a in nums1:
+            for b in nums2:
+                total = a + b
+                count[total] = count.get(total, 0) + 1
+
+        answer = 0
+
+      
+        for c in nums3:
+            for d in nums4:
+                total = c + d
+
+                if -total in count:
+                    answer += count[-total]
+
+        return answer
