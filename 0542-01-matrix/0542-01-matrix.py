4602c9655a900b930a446ef68febb95378a73f28
saimanupadala <saimanupadala@gmail.com>
2026-09-11 20:57:28 +0530

    Time: 103 ms (76.28%), Space: 20.9 MB (97.98%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    def updateMatrix(self, mat):
+        m = len(mat)
+        n = len(mat[0])
+
+        q = deque()
+
+        # Put all 0s in queue
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                else:
+                    mat[i][j] = -1
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while q:
+            r, c = q.popleft()
+
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+
+                if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] == -1:
+                    mat[nr][nc] = mat[r][c] + 1
+                    q.append((nr, nc))
+
+        return mat
