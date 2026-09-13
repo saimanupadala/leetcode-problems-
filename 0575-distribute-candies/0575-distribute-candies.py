8d5f3a34a9db5d8aa2d095b937258b6be8efd60e
saimanupadala <saimanupadala@gmail.com>
2026-09-13 16:21:42 +0530

    Time: 31 ms (23.39%), Space: 21.3 MB (11.22%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -0,0 +1,6 @@
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        n = len(candyType)
+        unique_types = len(set(candyType))
+        
+        return min(unique_types, n // 2)
